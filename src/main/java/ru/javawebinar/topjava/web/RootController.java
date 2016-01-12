3d7bc3e92bbaba1a3b9bbe09a1b315cfb797d5c6
stas.grishaev <oalximiko@gmail.com>
2016-01-13 00:07:13 +0300

    hw06 begin

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.javawebinar.topjava.LoggedUser;
+import ru.javawebinar.topjava.service.UserMealService;
 import ru.javawebinar.topjava.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +19,9 @@ import javax.servlet.http.HttpServletRequest;
 public class RootController {
     @Autowired
     private UserService service;
+
+    @Autowired
+    private UserMealService mealService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String root() {
@@ -35,5 +39,12 @@ public class RootController {
         int userId = Integer.valueOf(request.getParameter("userId"));
         LoggedUser.setId(userId);
         return "redirect:meals";
+    }
+
+
+    @RequestMapping(value = "/meals", method = RequestMethod.GET)
+    public String mealList(Model model) {
+        model.addAttribute("mealList", mealService.getAll(100000));
+        return "mealList";
     }
 }
