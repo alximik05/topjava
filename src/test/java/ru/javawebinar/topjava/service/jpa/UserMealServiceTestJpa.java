1f89faef8daf2b2462250c5ab8bf7abd9dba214e
stas.grishaev <oalximiko@gmail.com>
2016-01-12 09:27:01 +0300

    lesson6

---
+++ src/test/java/ru/javawebinar/topjava/service/jpa/UserMealServiceTestJpa.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.topjava.service.jpa;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.service.UserMealServiceTest;
+
+/**
+ * Created by alximik on 08/01/16.
+ */
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ActiveProfiles({Profiles.JPA,Profiles.POSTGRES})
+public class UserMealServiceTestJpa extends UserMealServiceTest {
+}
